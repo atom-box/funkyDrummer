a55a48508c45c7d75eebf32c6d923baeb5f80680
Evan Genest <genest.1@osu.edu>
2017-12-03 08:24:40 -0600

    Write my own GET statement for the backend to adjust the JSON and then implement their 2-line function from the answer key to make the PUT statement.

--- presetHandler.js
+++ presetHandler.js
@@ -1,8 +1,63 @@
 // Use this presets array inside your presetHandler
 const presets = require('./presets');
+const INDEXLIMIT = 15; // highest index allowed
+
+
+const createOrUpdatePreset = (index, array) => {
+// NOT MINE.  
+// WRITTEN BY UDACITY IN ANSWER KEY
+  if (!presets[index]) {
+    return;
+  }
+  presets[index] = array;
+  return presets[index];
+};
+
 
 // Complete this function:
-const presetHandler = () => {};
+const presetHandler = (requestS,indexN,	newPresetA) =>  	// Array 
+{
+	console.log(`Someone wants ${requestS},${indexN}, and ${newPresetA}  = + = + = + = ` );
+	let outGoing = [];
+
+	// GET begin
+	if (indexN < 0 || indexN > INDEXLIMIT)
+	{
+		outGoing[0] = 404; // PUT ERROR INTO RETURNARRAY
+	}
+	else if (requestS !== 'GET' && requestS !== 'PUT' )
+		outGoing[0] = 400;  // PUT ERROR INTO RETURNARRAY
+	else
+	{
+		outGoing[0] = 200;
+	}
+	if (outGoing[0] === 200 && requestS === 'GET') //RETURN FRESH BEATS
+	{
+		outGoing[1] = presets[indexN];
+	}
+	// end GET
+
+	// PUT begin
+	if (outGoing[0] === 200 && requestS === 'PUT') //STORE INCOMING BEATS
+	{
+		const array1 = createOrUpdatePreset(indexN, newPresetA);
+		outGoing[1] = array1;
+	}
+	//outgoing[1] = 'pangloss';  // WHEN I UNCOMMENT THIS LINE, IT CRASHES.  
+	console.log(`I will give them ${outGoing}  = + = + = + = ` );
+	return outGoing;
+};
+
+
+/*evTest!
+let testArray = presetHandler("Joy_Division", 50);
+console.log(testArray);
+testArray = presetHandler("New_Order", 5);
+console.log(testArray);
+testArray = presetHandler("PUT", 1, teams);
+console.log(testArray);
+*/
+
 
 // Leave this line so that your presetHandler function can be used elsewhere:
 module.exports = presetHandler;
